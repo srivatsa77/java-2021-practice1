0127dd18fc10108e7fceae531cac653dd7a28192
srivatsa77 <srivatsa.729@gmail.com>
2021-10-12 10:15:47 +0530

    12-10-21

--- src/difShirt.java
+++ src/difShirt.java
@@ -4,27 +4,26 @@ import java.util.Scanner;
 public class difShirt {
     public static void main(String args[]) throws IOException {
         Scanner sc = new Scanner(System.in);
-        int k, l, count = 0;
+        int k, l;
         k = sc.nextInt();
         int a[] = new int[k];
         for (int i = 0; i < k; i++) {
             l = sc.nextInt();
             a[i] = l;
         }
+        int finalCount = 0;
         for (int i = 0; i < k; i++) {
             int val = a[i];
-            for (int j = i + 1; j < k; j++) {           // C=[3, 2, 4, 1, 2, 3]
+            int count = 0;
+            for (int j = 0; j < k; j++) {           //a=[3, 2, 4, 1, 2, 3]
                 if (val == a[j]) {
-                    a[i] = 0;
-                    a[j] = 0;
+                    count = count + 1;
                 }
             }
-        }
-        for (int i = 0; i < k; i++) {
-            if (a[i] != 0) {
-                count = count + 1;
+            if (count == 1) {
+                finalCount = finalCount + 1;
             }
         }
-        System.out.print(count);
+        System.out.print(finalCount + " ");
     }
 }
