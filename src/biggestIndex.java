c20052b1ba2bc5eb9a1d2ed393312e81f65303f0
srivatsa77 <srivatsa.729@gmail.com>
2021-10-12 23:40:36 +0530

    12-10-21

--- src/biggestIndex.java
+++ src/biggestIndex.java
@@ -18,9 +18,10 @@ public class biggestIndex {
                 if (a[i] == 1)
                     b[i] = 1;
             }
-            for (int i = k - 1; i < k; i--) {
+            for (int i = k-1; i >= 0; i--) {
                 if (b[i] == 1)
                     System.out.print(i);
+                break;
             }
         }
     }
