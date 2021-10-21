e032edcd1936f9c083b28cf279b241e3bb18b0d9
srivatsa77 <srivatsa.729@gmail.com>
2021-10-21 21:56:31 +0530

    21-10-21

--- src/MissingNum.java
+++ src/MissingNum.java
@@ -4,19 +4,25 @@ import java.util.Scanner;
 public class MissingNum {
     public static void main(String args[]) throws IOException {
         Scanner sc = new Scanner(System.in);
-        int k, l, sum1 = 0, sum2 = 0;
-        k = sc.nextInt();
-        int a[] = new int[k];
-        int b[] = new int[k];
-        for (int i = 0; i < k - 1; i++) {                  //a  //1 6 5 4 2 7 = 25
-            l = sc.nextInt();                                              //b  //1 2 3 4 5 6 7 =28
-            a[i] = l;
-            sum1 = sum1 + l;
+        int t;
+        t = sc.nextInt();
+        while (t-- != 0) {
+            int k, l, sum1 = 0, sum2 = 0, sum;
+            k = sc.nextInt();
+            int a[] = new int[k-1];
+            int b[] = new int[k];
+            for (int i = 0; i < k - 1; i++) {                                  //a  //1 3 4 = 8
+                l = sc.nextInt();                                              //b  //1 2 3 4 =10
+                a[i] = l;
+                sum1 = sum1 + l;
+            }
+            for (int j = 0; j < k; j++) {
+                b[j] = j+1;
+                sum2 = sum2 + b[j];
+
+            }
+
+            System.out.println(sum2-sum1);
         }
-        for (int i = 0; i < k; i++) {
-            b[i] = i + 1;
-            sum2 = sum2 + i + 1;
-        }
-        System.out.println(sum2 - sum1);
     }
 }
